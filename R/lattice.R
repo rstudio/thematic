def2fa186c5aa143f9873e72facc3ccee68319db
Carson <cpsievert1@gmail.com>
2020-03-19 12:03:31 -0500

    fixup lattice params implementation

--- R/lattice.R
+++ R/lattice.R
@@ -34,24 +34,21 @@ lattice_par <- function(theme) {
   params <- list(
     # See figure 9.3 for an example of where grid gpar matters
     # http://lmdvr.r-forge.r-project.org/figures/figures.html
-    grid.pars = list(
-      # TODO: does font family need to go anywhere else?
-      fontfamily = font$family, col = fg, cex = font$scale
-    ),
+    grid.pars =         list(col = fg, fontfamily = font$family, cex = font$scale),
     background =        list(col = bg),
     reference.line =    list(col = bg),
     panel.background =  list(col = mix_colors(theme$bg, theme$fg, 0.1)),
     strip.background =  list(col = mix_colors(theme$bg, theme$fg, 0.2)),
     strip.border =      list(col = fg),
     axis.line =         list(col = fg),
-    axis.text =         list(col = fg, fontfamily = font$family),
+    axis.text =         list(col = fg),
     add.line =          list(col = fg),
-    add.text =          list(col = fg, fontfamily = font$family),
-    par.xlab.text =     list(col = fg, fontfamily = font$family),
-    par.ylab.text =     list(col = fg, fontfamily = font$family),
-    par.zlab.text =     list(col = fg, fontfamily = font$family),
-    par.main.text =     list(col = fg, fontfamily = font$family),
-    par.sub.text =      list(col = fg, fontfamily = font$family),
+    add.text =          list(col = fg),
+    par.xlab.text =     list(col = fg),
+    par.ylab.text =     list(col = fg),
+    par.zlab.text =     list(col = fg),
+    par.main.text =     list(col = fg),
+    par.sub.text =      list(col = fg),
     box.3d =            list(col = fg),
     plot.polygon =      list(border = fg),
     superpose.polygon = list(border = fg),
@@ -63,24 +60,25 @@ lattice_par <- function(theme) {
   # 'stroke' accent and one for fill accent
   accent <- rep(theme$accent, length.out = 2)
   if (sum(is.na(accent)) == 0) {
-    params$plot.line <-     list(col = accent[[1]])
-    params$plot.symbol <-   list(col = accent[[1]])
-    params$dot.symbol <-    list(col = accent[[1]])
-    params$box.rectangle <- list(col = accent[[1]])
-    params$box.umbrella <-  list(col = accent[[1]])
-    params$plot.polygon <-  list(col = accent[[2]])
-    params$grid.pars <-     list(fill = accent[[2]])
+    params$plot.line$col <-     accent[[1]]
+    params$plot.symbol$col <-   accent[[1]]
+    params$dot.symbol$col <-    accent[[1]]
+    params$box.rectangle$col <- accent[[1]]
+    params$box.umbrella$col <-  accent[[1]]
+    params$plot.polygon$col <-  accent[[2]]
+    params$grid.pars$fill <-    accent[[2]]
   }
 
   qualitative <- theme$qualitative
   if (sum(is.na(qualitative)) == 0) {
     # I'm not in love with the idea of this; but alas, it's consistent with lattice's default
     region_pal <- grDevices::colorRampPalette(c(qualitative[[1]], "white", qualitative[[2]]))
-    params$strip.shingle <-     list(col = qualitative)
-    params$regions <-           list(col = region_pal(100))
-    params$superpose.line <-    list(col = qualitative)
-    params$superpose.symbol <-  list(col = qualitative, fill = qualitative)
-    params$superpose.polygon <- list(col = qualitative)
+    params$strip.shingle$col <-     qualitative
+    params$regions$col <-           region_pal(100)
+    params$superpose.line$col <-    qualitative
+    params$superpose.symbol$col <-  qualitative
+    params$superpose.symbol$fill <- qualitative
+    params$superpose.polygon$col <- qualitative
   }
 
   params
