67c894f2e3bcced3d9cfc669faa792e4269cb0fc
Carson <cpsievert1@gmail.com>
2020-04-10 17:32:48 -0500

    par() and palette() open a new device if there isn't one open

--- R/base.R
+++ R/base.R
@@ -1,12 +1,16 @@
 base_palette_set <- function(theme = .globals$theme) {
   base_palette_restore()
   codes <- theme$qualitative
-  .globals$base_palette <- if (isTRUE(is.na(codes))) palette() else palette(codes)
+  .globals$base_palette <- if (isTRUE(is.na(codes))) {
+    palette_no_new_device()
+  } else {
+    palette_no_new_device(codes)
+  }
 }
 
 base_palette_restore <- function() {
   if (is.null(.globals$base_palette)) return()
-  palette(.globals$base_palette)
+  palette_no_new_device(.globals$base_palette)
   rm("base_palette", envir = .globals)
 }
 
@@ -15,11 +19,11 @@ base_params_set <- function(theme = .globals$theme) {
   params <- list()
   bg <- theme$bg
   if (length(bg)) {
-    params <- c(params, par(bg = bg))
+    params <- c(params, par_no_new_device(bg = bg))
   }
   fg <- theme$fg
   if (length(fg)) {
-    params <- c(params, par(
+    params <- c(params, par_no_new_device(
       fg = fg,
       col.axis = fg,
       col.lab = fg,
@@ -29,7 +33,7 @@ base_params_set <- function(theme = .globals$theme) {
   }
   font <- theme$font
   if (length(font$family)) {
-    params <- c(params, par(
+    params <- c(params, par_no_new_device(
       family = font$family,
       cex.axis = font$scale,
       cex.lab = font$scale,
@@ -42,6 +46,24 @@ base_params_set <- function(theme = .globals$theme) {
 
 base_params_restore <- function() {
   if (is.null(.globals$base_params)) return()
-  do.call(par, .globals$base_params)
+  do.call(par_no_new_device, .globals$base_params)
   rm("base_params", envir = .globals)
 }
+
+par_no_new_device <- function(...) {
+  dev <- dev.cur()
+  if (dev.cur() == 1) {
+    on.exit(dev.off(), add = TRUE)
+  }
+  par(...)
+}
+
+palette_no_new_device <- function(...) {
+  dev <- dev.cur()
+  if (dev.cur() == 1) {
+    on.exit(dev.off(), add = TRUE)
+  }
+  palette(...)
+}
+
+
