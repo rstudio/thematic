5492d037c0a04e1230f0c1912169613929e247b0
Carson <cpsievert1@gmail.com>
2020-06-02 18:58:34 -0500

    add internal helper function for viewing shinytest differences

---
+++ R/view-shinytest.R
@@ -0,0 +1,22 @@
+# view all shinytest differences inside package tests
+view_shinytest_diffs <- function(path = ".", suffix = "auto", ...) {
+  if (identical(suffix, "auto")) {
+    branch <- system("git rev-parse --abbrev-ref HEAD", intern = TRUE)
+    pieces <- strsplit(branch, "-")[[1]]
+    platform <- pieces[[length(pieces)]]
+    suffix <- switch(
+      platform,
+      Windows = "win",
+      macOS = "mac",
+      Linux = "linux",
+      ""
+    )
+  }
+  test_path <- file.path(path, "tests", "testthat")
+  test_path <- normalizePath(test_path, mustWork = TRUE)
+  test_dirs <- grep("tests/shinytest/.*-current", list.dirs(test_path), value = TRUE)
+  app_dirs <- dirname(dirname(dirname(test_dirs)))
+  for (dir in app_dirs) {
+    shinytest::viewTestDiff(appDir = dir, suffix = suffix, interactive = TRUE, ...)
+  }
+}
