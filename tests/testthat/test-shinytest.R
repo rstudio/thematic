0ddd172d4c65bf4c61111591ddee329500fad763
Carson <cpsievert1@gmail.com>
2020-06-03 13:05:31 -0500

    add test for shiny: runtime

--- tests/testthat/test-shinytest.R
+++ tests/testthat/test-shinytest.R
@@ -46,6 +46,7 @@ test_that("Auto theming in shiny works", {
   expect_app_doppelganger("auto_theme_shiny/lattice")
   expect_app_doppelganger("auto_theme_shiny/ggplot2")
   expect_app_doppelganger("auto_theme_shiny/local")
+  expect_app_doppelganger("shiny_runtime")
 })
 
 test_that("Custom fonts with Cairo package", {
