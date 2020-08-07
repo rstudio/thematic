d3f3f73d887854ad6321eba9fb139fd4419f3e6f
Carson <cpsievert1@gmail.com>
2020-08-07 17:39:15 -0500

    Leverage the fact that thematic_get_option() resolves auto values

--- R/knitr.R
+++ R/knitr.R
@@ -4,33 +4,13 @@
 knitr_dev_args_set <- function() {
   if (!isTRUE(getOption("knitr.in.progress"))) return()
 
-  # resolve `bg = 'auto'`
-  bg <- .globals$theme$bg
-
-  # Resolve bg = "auto" in a similar manner to auto_resolve_theme()
-  # (i.e., allow auto values to be repeatedly resolved)
-  if (is_auto(bg)) {
-    bg <- auto_config_get()[["bg"]] %||%
-      bs_theme_colors()[["bg"]] %||%
-      bg
-    if (isTRUE("auto" == bg)) {
-      maybe_warn(
-        "Couldn't detect an 'auto' bg color for the knitr dev",
-        id = "knitr-device-bg-auto"
-      )
-      bg <- "white"
-    } else {
-      bg <- parse_any_color(bg)
-    }
-    bg <- as_auto(bg)
-  }
-
   dev <- knitr::opts_chunk$get("dev")
   old_args <- knitr::opts_chunk$get("dev.args")
   # Support ragg device if and when it's officially supported
   # https://github.com/yihui/knitr/pull/1834
   new_args <- rlang::set_names(
-    list(bg), if (identical(dev, "ragg_png")) "background" else "bg"
+    list(thematic_get_option("bg", "white")),
+    if (identical(dev, "ragg_png")) "background" else "bg"
   )
   if (isTRUE(dev %in% names(old_args))) {
     new_args <- rlang::set_names(list(new_args), dev)
