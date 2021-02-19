1cf2120ac3d437664c6e46d4bbea1e3b20677867
Carson Sievert <cpsievert1@gmail.com>
2021-02-18 18:00:58 -0600

    Get thematic working with packages that use registerS3method() inside .onLoad() to register ggplot2 methods (#91)
    * Close #90: Get thematic working with packages that use registerS3method() inside .onLoad() to register ggplot2 methods
    
    * Improve comment and also ensure S3methods matrix when restoring previous ggplot_build
    
    * New ragg baselines
    
    * update news; approve ragg mac baseline

--- R/ggplot.R
+++ R/ggplot.R
@@ -18,6 +18,7 @@ ggplot_build_set <- function() {
   ggplot_build <- getFromNamespace("ggplot_build", "ggplot2")
   .globals$ggplot_build <- getFromNamespace("ggplot_build.ggplot", "ggplot2")
   assign_in_namespace <- assignInNamespace
+  ensure_s3_methods_matrix()
   assign_in_namespace("ggplot_build.ggplot", ggthematic_build, "ggplot2")
 }
 
@@ -25,9 +26,22 @@ ggplot_build_restore <- function() {
   if (is.function(.globals$ggplot_build)) {
     ggplot_build <- getFromNamespace("ggplot_build", "ggplot2")
     assign_in_namespace <- assignInNamespace
+    ensure_s3_methods_matrix()
     assign_in_namespace("ggplot_build.ggplot", .globals$ggplot_build, "ggplot2")
     rm("ggplot_build", envir = .globals)
   }
+}
+
+# When registerS3method() is called in an onLoad() hook, it causes
+# assignInNamespace() to no longer work for S3 methods because this
+# S3methods namespace info gets coerced into a list
+# (which assignInNamespace() isn't expecting). Thus, here, we make
+# sure that the info is a matrix before calling assignInNamespace()
+# https://github.com/rstudio/thematic/issues/90#issuecomment-780224962
+ensure_s3_methods_matrix <- function(pkg = "ggplot2") {
+  S3 <- .getNamespaceInfo(asNamespace(pkg), "S3methods")
+  S3 <- matrix(as.character(S3), nrow = nrow(S3), ncol = ncol(S3))
+  setNamespaceInfo(asNamespace(pkg), "S3methods", S3)
 }
 
 # N.B. If you make changes here, plotly might have to as well!
