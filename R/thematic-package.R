7082e5362d675389d4bcb85679e53f028787fd39
Carson <cpsievert1@gmail.com>
2023-08-08 15:27:29 -0500

    rstudio -> posit emails; use_package_doc()

--- R/thematic-package.R
+++ R/thematic-package.R
@@ -1,7 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @importFrom graphics par plot plot.new
 #' @importFrom grid grid.newpage grid.draw grid.text gpar seekViewport pushViewport upViewport
 #' @importFrom grDevices col2rgb dev.new dev.cur dev.off dev.set devAskNewPage palette colorRampPalette
 #' @importFrom utils capture.output download.file unzip packageVersion modifyList getFromNamespace getS3method assignInNamespace
 #' @importFrom rlang is_installed list2 %|%
 #' @importFrom rstudioapi getThemeInfo versionInfo readRStudioPreference
+## usethis namespace: end
 NULL
